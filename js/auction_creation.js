6fe05a26f7403d8c6ca5c4dc2508c75b6c4a7dcc
SoleneK <solene.krebs@gmail.com>
2018-02-15 21:40:13 +0100

    Suppression des lignes inutiles dans un fichier

--- js/auction_creation.js
+++ js/auction_creation.js
@@ -2,17 +2,6 @@ $(function() {
 	$('[data-toggle="tooltip"]').tooltip();
 
 	$('main form').on('submit', function(event) {
-		// Vérifier que le pseudo ne contient que des lettres, chiffres et _
-		/*if ($('#inscription_pseudo').val().search(/\W./) !== -1)
-			var error_message = 'Le pseudo ne doit contenir que des lettres, des chiffres et des _';
-		// Vérifier que les deux mots de passe saisis sont identiques
-		else if ($('#inscription_password_1').val() !== $('#inscription_password_2').val())
-			var error_message = 'Les deux mots de passe ne sont pas identiques';*/
-
-		// Vérifier que le fichier envoyé est une image
-
-		// Vérifier la date de fin
-
 		// Vérifier que le prix est positif
 		if ($('#auction_start_bid').val() < 0)
 			var error_message = 'Le prix de départ doit être positif';
